7d763d49bf0de523d1f5bc36470d2fe7abfc9df7
jcg0 <jg.awsprod@gmail.com>
2022-12-28 22:22:47 -0500

    created addManager function which uses inquirer for the user to input answers and then when all answers are selected it creates a new manager array and adds it to the teamArr

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const generateHTML = require("./src/generateTeam");
+
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+const teamArr = [];
+const addManager = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Manager name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Manager's ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Manager's email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the Manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      const { name, id, email, officeNumber } = answers;
+      const manager = new Manager(name, id, email, officeNumber);
+      teamArr.push(manager);
+      console.log(manager);
+    });
+};
